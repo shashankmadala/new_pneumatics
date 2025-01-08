ec47ba07a0a3879fa191faa4ec4e97b4bad645dd
peter-wolfe <wolfe@pobox.com>
2025-01-07 21:29:39 -0500

    more changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public final class Constants {
     public static final class PneumaticsConstants {
-        public static final int PCM_CAN_ID = 5; // Change this to match your PCM CAN ID
-        public static final int FORWARD_CHANNEL = 0; // Change to match your solenoid forward channel
+        public static final int PCM_CAN_ID = 5; 
+        public static final int FORWARD_CHANNEL = 1; // solenoid forward
         public static final int REVERSE_CHANNEL = 1; // Change to match your solenoid reverse channel
         public static final int CONTROLLER_PORT = 2; // usb port
         public static final int PISTON_BUTTON = 1; // xbox
--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import frc.robot.Constants.PneumaticsConstants;
@@ -10,26 +12,17 @@ import frc.robot.Constants.PneumaticsConstants;
 public class Pneumatics extends SubsystemBase {
     // Use CTREPCM as the module type for CTRE PCM
     private final Compressor compressor = new Compressor(PneumaticsConstants.PCM_CAN_ID, PneumaticsModuleType.CTREPCM);
-    private final DoubleSolenoid piston;
+    private final PneumaticsControlModule pcm = new PneumaticsControlModule(PneumaticsConstants.PCM_CAN_ID);
+    private final Solenoid piston;
     
     public Pneumatics() {
         // Create double solenoid with CTRE PCM
-        piston = new DoubleSolenoid(
+        piston = new Solenoid(
             PneumaticsModuleType.CTREPCM,
-            PneumaticsConstants.FORWARD_CHANNEL,
-            PneumaticsConstants.REVERSE_CHANNEL
-        );
+            PneumaticsConstants.FORWARD_CHANNEL        );
         
         // Enable compressor
         compressor.enableDigital();
-    }
-    
-    public void extendPiston() {
-        piston.set(kForward);
-    }
-    
-    public void retractPiston() {
-        piston.set(kReverse);
     }
     
     public void togglePiston() {
