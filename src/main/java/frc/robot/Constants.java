96efc09132d859bdc69e6fb0894ec9d2e2ccf43a
peter-wolfe <wolfe@pobox.com>
2025-01-07 20:27:09 -0500

    changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,10 +2,10 @@ package frc.robot;
 
 public final class Constants {
     public static final class PneumaticsConstants {
-        public static final int PCM_CAN_ID = 1; // Change this to match your PCM CAN ID
+        public static final int PCM_CAN_ID = 5; // Change this to match your PCM CAN ID
         public static final int FORWARD_CHANNEL = 0; // Change to match your solenoid forward channel
         public static final int REVERSE_CHANNEL = 1; // Change to match your solenoid reverse channel
-        public static final int CONTROLLER_PORT = 0; // USB port for Xbox controller
-        public static final int PISTON_BUTTON = 1; // Xbox controller A button
+        public static final int CONTROLLER_PORT = 2; // usb port
+        public static final int PISTON_BUTTON = 1; // xbox
     }
 }
